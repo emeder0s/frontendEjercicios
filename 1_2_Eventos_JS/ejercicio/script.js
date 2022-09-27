85f25eb01e3780bd0950520236eb98abea96fd08
Elena Mederos <elenamc91@hotmail.com>
2022-09-27 11:25:39 +0200

    Añadidos ejemplos y ejercicos de eventos JS

---
+++ 1_2_Eventos_JS/ejercicio/script.js
@@ -0,0 +1,21 @@
+
+// función que sirve para ocultar y mostrar
+function muestraOculta(num) {
+    var parrafo = document.getElementById("contenidos_" + num);
+    var enlace = document.getElementById("enlace_" + num);
+    if (parrafo.style.visibility == "" || parrafo.style.visibility == "visible") {
+        parrafo.style.visibility = "hidden";
+        enlace.innerText = "Mostrar contenido";
+    }
+    else {
+        parrafo.style.visibility = "visible";
+        enlace.innerText = "Ocultar contenido";
+    }
+}
+
+window.onload = () => {
+    var numEnlaces = document.getElementsByTagName("a").length;
+    for(let i=1; i <=numEnlaces; i++) {
+        document.getElementById("enlace_" + i).setAttribute("onclick", "muestraOculta("+ i +")");
+    }
+}
